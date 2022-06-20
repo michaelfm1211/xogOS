21ca5243972104cd073843be4ff09837d6b26620
Michael M <mic@hael.xyz>
2022-06-20 11:37:30 -0500

    remove ugly comments

--- print.asm
+++ print.asm
@@ -1,31 +1,28 @@
 ; taken from somewhere else, but I forget
 
-; print function
 print:
     pusha
 start:
-    mov al, [bx] ; bx parameter is a pointer to the string
-    cmp al, 0 ; if the character is 0,
-    je done	; we're done
+    mov al, [bx]
+    cmp al, 0
+    je done
 
-    mov ah, 0x0e	; change to TTY mode for int 0x10
-    int 0x10	; BIOS interrupt for print character in al
+    mov ah, 0x0e
+    int 0x10
 
-    ; increment pointer and continue loop
     add bx, 1
     jmp start
 done:
     popa
     ret
 
-; print new line
 print_newline:
     pusha
     
     mov ah, 0x0e
-    mov al, 0x0a ; newline
+    mov al, 0x0a
     int 0x10
-    mov al, 0x0d ; carriage return
+    mov al, 0x0d
     int 0x10
     
     popa
